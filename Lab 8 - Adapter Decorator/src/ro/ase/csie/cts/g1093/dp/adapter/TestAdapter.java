a9e62797e99db788db5975e079b4e715430dcc21
gvlad44 <gont.vlad@hotmail.com>
2021-04-15 14:26:01 +0300

    lab 8 break

---
+++ Lab 8 - Adapter Decorator/src/ro/ase/csie/cts/g1093/dp/adapter/TestAdapter.java
@@ -0,0 +1,34 @@
+package ro.ase.csie.cts.g1093.dp.adapter;
+
+import java.util.ArrayList;
+
+public class TestAdapter {
+
+	public static void main(String[] args) {
+
+		ACMECharacter dragon = new FantasyCharacter("Geany", 1000);
+		dragon.move();
+		dragon.takeAHit(200);
+		dragon.heal(100);
+		
+		DisneyActions donald = new DonaldDuck("Donald Duck", 500);
+		donald.changeLocation(100, 50);
+		donald.losePower(150);
+		donald.recoverPower(80);
+
+		//use the adapter
+		ArrayList<ACMECharacter> characters = new ArrayList<>();
+		characters.add(dragon);
+		DisneyToACMEAdapter donaldAdapter = new DisneyToACMEAdapter(donald);
+		characters.add(donaldAdapter);
+		
+		System.out.println("---------------------------------------");
+		
+		for(ACMECharacter hero: characters) {
+			hero.move();
+			hero.takeAHit(50);
+		}
+		
+	}
+
+}
