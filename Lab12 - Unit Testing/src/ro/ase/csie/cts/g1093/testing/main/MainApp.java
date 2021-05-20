aa882a0625678f7fedc04246e04f8b71f082bede
gvlad44 <gont.vlad@hotmail.com>
2021-05-20 15:34:18 +0300

    lab 12 final

---
+++ Lab12 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/main/MainApp.java
@@ -0,0 +1,15 @@
+package ro.ase.csie.cts.g1093.testing.main;
+
+import ro.ase.csie.cts.g1093.testing.models.MathOperations;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("The application is running...");
+
+
+		
+	}
+
+}
---
+++ Lab12 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/models/MathOperations.java
@@ -0,0 +1,18 @@
+package ro.ase.csie.cts.g1093.testing.models;
+
+import ro.ase.csie.cts.g1093.testing.exceptions.DivisionBy0Exception;
+
+public class MathOperations {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static double divide(int a, int b) throws DivisionBy0Exception {
+		if (b == 0) {
+			throw new DivisionBy0Exception();
+		}
+		return a / b;
+	}
+
+}
